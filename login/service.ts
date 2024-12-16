d55d09e8ad2c1d4f2a03aa60c4bf63bdca7604a3
Andrew Wittrock <andy@wittrock.us>
2024-12-16 00:40:03 -0600

    Send permissions with login response

--- login/service.ts
+++ login/service.ts
@@ -1,16 +1,19 @@
 import jwt from "jsonwebtoken";
 import { CodedPromise } from "serverless-api-boilerplate";
 import { secret } from "../../../config";
-import { User } from "../user/service";
 import { ILoginResponse } from "../../uac-shared/login/types";
+import { User } from "../user/service";
 
 export const Login = {
     login: (userName: string, password:string):Promise<ILoginResponse> => CodedPromise<ILoginResponse>((resolve, reject, fail, error, notFound) => {
-        User.loadUnsafeByName(userName).then(user => {
+        User.loadUnsafeByName(userName).then(async user => {
             if(User.hashPassword(password) === user.passwordHash) {
+                const userId = user.id;
+                const permissions = await User.permissions.get(user.id);
                 resolve({
                     user: User.makeSafe(user),
-                    loginToken: jwt.sign({userId: user.id}, secret),
+                    permissions,
+                    loginToken: jwt.sign({userId}, secret),
                 });
             } else {
                 fail("Invalid credentials");
