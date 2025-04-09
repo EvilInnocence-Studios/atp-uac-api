7bdaa860c85896bafe8a1806898ff382e048f6f6
Andrew Wittrock <andy@wittrock.us>
2025-04-08 22:58:48 -0500

    Fix migration table names

--- migrations/util.ts
+++ migrations/util.ts
@@ -16,14 +16,14 @@ export const insertRolePermissions = async (db: Knex, rolePermissions: Array<{ro
     const roleIds = await db.select("id").from("roles").whereIn("name", rolePermissions.map(r => r.roleName));
     const permissionIds = await db.select("id").from("permissions").whereIn("name", rolePermissions.map(r => r.permissionName));
     const rolePermissionData = roleIds.flatMap(role => permissionIds.map(permission => ({roleId: role.id, permissionId: permission.id})));
-    await db.insert(rolePermissionData).into("role_permissions");
+    await db.insert(rolePermissionData).into("rolePermissions");
 };
 
 export const insertUserRoles = async (db: Knex, userRoles: Array<{userName: string, roleName: string}>):Promise<void> => {
     const userIds = await db.select("id").from("users").whereIn("userName", userRoles.map(r => r.userName));
     const roleIds = await db.select("id").from("roles").whereIn("name", userRoles.map(r => r.roleName));
     const userRoleData = userIds.flatMap(user => roleIds.map(role => ({userId: user.id, roleId: role.id})));
-    await db.insert(userRoleData).into("user_roles");
+    await db.insert(userRoleData).into("userRoles");
 };
 
 
