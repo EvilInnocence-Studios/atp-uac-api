f62db3dfad0bec2b191a28e121407b7a0e0f4887
Andrew Wittrock <andy@wittrock.us>
2025-11-23 23:57:57 -0600

    Set priority on init migration

--- migrations/00-init/index.ts
+++ migrations/00-init/index.ts
@@ -1,37 +1,37 @@
 import dayjs from "dayjs";
 import { database } from "../../../core/database";
-import { IMigration } from "../../../core/database.d";
 import { User } from "../../user/service";
 import { permissionsTable, rolePermissionsTable, rolesTable, userRolesTable, usersTable } from "../tables";
 import { insertRolePermissions, insertUsers, insertUserRoles, insertRoles, insertPermissions } from "../util";
+import { IMigration } from "@core/dbMigrations";
 
 const db = database();
 
-const userBlanks = {firstName: "", lastName: "", prefix: "", suffix: "", createdAt: dayjs().toISOString()};
+const userBlanks = { firstName: "", lastName: "", prefix: "", suffix: "", createdAt: dayjs().toISOString() };
 export const users = [
-    {userName: "admin",  email: "admin@example.com", passwordHash: User.hashPassword("admin"), mustUpdatePassword: true, ...userBlanks},
-    {userName: "public", email: "",                  passwordHash: "",                         mustUpdatePassword: false, ...userBlanks},
+    { userName: "admin", email: "admin@example.com", passwordHash: User.hashPassword("admin"), mustUpdatePassword: true, ...userBlanks },
+    { userName: "public", email: "", passwordHash: "", mustUpdatePassword: false, ...userBlanks },
 ];
 
 const roles = [
-    { name: "SuperUser", description: "SuperUser role"    },
-    { name: "Public",    description: "Non-logged in user"},
+    { name: "SuperUser", description: "SuperUser role" },
+    { name: "Public", description: "Non-logged in user" },
 ];
 
 const permissions = [
-    { name: "user.view",            description: "Can view users"        },
-    { name: "user.update",          description: "Can update users"      },
-    { name: "user.create",          description: "Can create users"      },
-    { name: "user.delete",          description: "Can delete users"      },
-    { name: "user.admin",           description: "Can administer users"  },
-    { name: "role.view",            description: "Can view roles"        },
-    { name: "role.update",          description: "Can update roles"      },
-    { name: "role.create",          description: "Can create roles"      },
-    { name: "role.delete",          description: "Can delete roles"      },
-    { name: "permission.view",      description: "Can view permissions"  },
-    { name: "permission.update",    description: "Can update permissions"},
-    { name: "permission.create",    description: "Can create permissions"},
-    { name: "permission.delete",    description: "Can delete permissions"},
+    { name: "user.view", description: "Can view users" },
+    { name: "user.update", description: "Can update users" },
+    { name: "user.create", description: "Can create users" },
+    { name: "user.delete", description: "Can delete users" },
+    { name: "user.admin", description: "Can administer users" },
+    { name: "role.view", description: "Can view roles" },
+    { name: "role.update", description: "Can update roles" },
+    { name: "role.create", description: "Can create roles" },
+    { name: "role.delete", description: "Can delete roles" },
+    { name: "permission.view", description: "Can view permissions" },
+    { name: "permission.update", description: "Can update permissions" },
+    { name: "permission.create", description: "Can create permissions" },
+    { name: "permission.delete", description: "Can delete permissions" },
 ];
 
 const rolePermissions = [
@@ -52,11 +52,15 @@ const rolePermissions = [
 ];
 
 export const userRoles = [
-    { userName: "admin",  roleName: "SuperUser" },
-    { userName: "public", roleName: "Public"    },
+    { userName: "admin", roleName: "SuperUser" },
+    { userName: "public", roleName: "Public" },
 ];
 
-export const init:IMigration = {
+export const init: IMigration = {
+    name: "init-uac",
+    module: "uac",
+    description: "Install the uac module",
+    order: 0,
     down: () => db.schema
         // Drop tables in reverse order to ensure foreign key constraints don't stop things
         .dropTableIfExists("userRoles")
@@ -72,7 +76,6 @@ export const init:IMigration = {
         .createTable("permissions", permissionsTable)
         .createTable("rolePermissions", rolePermissionsTable)
         .createTable("userRoles", userRolesTable),
-    priority: 0,
     initData: async () => {
         await insertUsers(db, users);
         await insertRoles(db, roles);
